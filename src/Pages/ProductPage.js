39d5b5331a43181ab398e0e2ea9f17331c7be4c7
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-05 19:20:46 +0530

    Product

--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./ProductPage.css";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function ProductPage() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [data, setData] = useState();
+  const [quantity, setQuantity] = useState(1);
+  const [price, setPrice] = useState(0);
+  const [firstprice, setFirstprice] = useState(0);
   useEffect(() => {
     axios
       .post(`https://e1commerce.herokuapp.com/api/product/${id}/`, {
@@ -13,15 +17,62 @@ function ProductPage() {
       })
       .then((Response) => {
         setData(Response.data);
+        setPrice(Response.data.price);
+        setFirstprice(Response.data.price);
       })
       .catch((error) => {
         alert(error.data);
       });
-  }, []);
+  }, [id]);
   console.log(data);
   return (
     <div>
-      <h1>Hello </h1>
+      <p
+        onClick={() => {
+          navigate("/");
+        }}
+        className="goback"
+      >
+        GO BACK
+      </p>
+      <div className="details">
+        <img src={data ? data.image : ""} alt="Product" />
+        <div classname="namedesc">
+          <h5>{data ? data.name : ""}</h5>
+          <p>{data ? data.product_reviews.length : ""}</p>
+          <p>Price: {data ? data.price : ""}</p>
+          <p>{data ? data.desciption : ""}</p>
+        </div>
+        <div className="cart">
+          <p>Price: {price ? price : ""}</p>
+          <p>Status: {data ? data.status : ""}</p>
+          <div className="quantity">
+            <p>Qty</p>
+            <div className="operator">
+              <p
+                onClick={() => {
+                  if (quantity !== 1) {
+                    setPrice(firstprice * (quantity - 1));
+                    setQuantity(quantity - 1);
+                  }
+                }}
+              >
+                -
+              </p>
+              <p>{quantity}</p>
+              <p
+                onClick={() => {
+                  setPrice((quantity + 1) * firstprice);
+                  setQuantity(quantity + 1);
+                }}
+              >
+                +
+              </p>
+            </div>
+          </div>
+          <button>ADD TO CART</button>
+        </div>
+      </div>
     </div>
   );
 }
