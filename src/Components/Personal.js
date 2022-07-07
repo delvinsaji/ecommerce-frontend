41e8271d95eee9b6974de4399d4d19d6008f34dd
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-07 14:49:43 +0530

    personal

--- src/Components/Personal.js
+++ src/Components/Personal.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Personal.css";
 
 function Personal() {
-  return <div></div>;
+  return (
+    <div className="personal">
+      <h3 className="infohead">ACCOUNT INFORMATION</h3>
+    </div>
+  );
 }
 
 export default Personal;
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -6,36 +6,38 @@ function Profile() {
   const navigate = useNavigate();
 
   return (
-    <div className="profile">
-      <h3>MY ACCOUNT</h3>
-      <div className="sections">
-        <p
-          className="y"
-          onClick={() => {
-            navigate("personal");
-          }}
-        >
-          Personal Details
-        </p>
-        <p
-          className="y"
-          onClick={() => {
-            navigate("address");
-          }}
-        >
-          My Address
-        </p>
-        <p
-          className="y"
-          onClick={() => {
-            navigate("myproducts");
-          }}
-        >
-          My Products
-        </p>
-      </div>
-      <div className="ma">
-        <Outlet></Outlet>
+    <div>
+      <h3 className="acc">MY ACCOUNT</h3>
+      <div className="profile">
+        <div className="sections">
+          <p
+            className="y"
+            onClick={() => {
+              navigate("personal");
+            }}
+          >
+            Personal Details
+          </p>
+          <p
+            className="y"
+            onClick={() => {
+              navigate("address");
+            }}
+          >
+            My Address
+          </p>
+          <p
+            className="y"
+            onClick={() => {
+              navigate("myproducts");
+            }}
+          >
+            My Products
+          </p>
+        </div>
+        <div className="ma">
+          <Outlet></Outlet>
+        </div>
       </div>
     </div>
   );
