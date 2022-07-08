03216bf66dd9adeb6544d1a3c8b7d746f8e60585
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-08 13:00:06 +0530

    myproducts

--- src/Components/Myproducts.js
+++ src/Components/Myproducts.js
@@ -19,31 +19,44 @@ function Myproducts() {
       <div className="addproduct">
         <h5>ADD PRODUCT</h5>
         <div className="prodinfo">
-          <div className="prodfield">
-            <p>Product Name</p>
-            <input type="text" placeholder="Product Name"></input>
+          <div className="prod1">
+            <div className="prodfield">
+              <p>Product Name</p>
+              <input type="text" placeholder="Product Name"></input>
+            </div>
+            <div className="prodfield">
+              <p>Description</p>
+              <textarea placeholder="Description"></textarea>
+            </div>
           </div>
-          <div className="prodfield">
-            <p>Product Name</p>
-            <input type="text" placeholder="Product Name"></input>
-          </div>
-          <div className="prodfield">
-            <p>Product Name</p>
-            <input type="text" placeholder="Product Name"></input>
-          </div>
-          <div className="prodfield">
-            <p>Product Name</p>
-            <input type="text" placeholder="Product Name"></input>
-          </div>
-          <div className="prodfield">
-            <p>Product Name</p>
-            <input type="text" placeholder="Product Name"></input>
-          </div>
-          <div className="prodfield">
-            <p>Product Name</p>
-            <input type="text" placeholder="Product Name"></input>
+          <div className="prod2">
+            <div className="prodfield">
+              <p>Price</p>
+              <input type="text" placeholder="Price Name"></input>
+            </div>
+
+            <div className="prodfield">
+              <p>Image Url</p>
+              <input type="text" placeholder="URL"></input>
+            </div>
+            <div className="dropdown">
+              <div className="prodfield">
+                <p>Status</p>
+                <select>
+                  <option value="Available">Available</option>
+                  <option value="Not In Stock">Not In Stock</option>
+                </select>
+              </div>
+              <div className="prodfield">
+                <p>Category</p>
+                <select>
+                  <option value="Smartphone">Smartphone</option>
+                </select>
+              </div>
+            </div>
           </div>
         </div>
+        <button>ADD PRODUCT</button>
       </div>
     </div>
   );
