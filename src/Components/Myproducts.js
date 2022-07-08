4e82a565a16f3d72a94d60cf67eabf445d111945
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-08 18:41:32 +0530

    myproducts

--- src/Components/Myproducts.js
+++ src/Components/Myproducts.js
@@ -1,11 +1,127 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Myproducts.css";
 
+function Productform(props) {
+  const [name, setName] = useState();
+  const [price, setPrice] = useState();
+  const [desc, setDesc] = useState();
+  const [image, setImage] = useState();
+  const [category, setCategory] = useState();
+  const [status, setStatus] = useState();
+
+  useEffect(() => {
+    if (props.details != "") {
+      setName(props.details[0]);
+      setPrice(props.details[1]);
+      setDesc(props.details[2]);
+      setImage(props.details[3]);
+      setStatus(props.details[4]);
+      setCategory(props.details[5]);
+    }
+  }, []);
+
+  return (
+    <div className="addproduct">
+      <h5>{props.purpose} PRODUCT</h5>
+      <div className="prodinfo">
+        <div className="prod1">
+          <div className="prodfield">
+            <p>Product Name</p>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              placeholder="Product Name"
+            ></input>
+          </div>
+          <div className="prodfield">
+            <p>Description</p>
+            <textarea
+              value={desc}
+              onChange={(e) => {
+                setDesc(e.target.value);
+              }}
+              placeholder="Description"
+            ></textarea>
+          </div>
+        </div>
+        <div className="prod2">
+          <div className="prodfield">
+            <p>Price</p>
+            <input
+              type="text"
+              value={price}
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+              placeholder="Price Name"
+            ></input>
+          </div>
+
+          <div className="prodfield">
+            <p>Image Url</p>
+            <input
+              type="text"
+              value={image}
+              onChange={(e) => {
+                setImage(e.target.value);
+              }}
+              placeholder="URL"
+            ></input>
+          </div>
+          <div className="dropdown">
+            <div className="prodfield">
+              <p>Status</p>
+              <select
+                value={status}
+                onChange={(e) => {
+                  setStatus(e.target.value);
+                }}
+              >
+                <option value="Available">Available</option>
+                <option value="Not In Stock">Not In Stock</option>
+              </select>
+            </div>
+            <div className="prodfield">
+              <p>Category</p>
+              <select
+                value={category}
+                onChange={(e) => {
+                  setCategory(e.target.value);
+                }}
+              >
+                <option value="Smartphone">Smartphone</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={() => {
+          if (props.purpose === "UPDATE") {
+            props.status(false);
+          }
+        }}
+      >
+        {props.purpose} PRODUCT
+      </button>
+    </div>
+  );
+}
+
 function Myproducts() {
+  const [sts, setSts] = useState(false);
   return (
     <div>
       <h3 className="infohead">MY PRODUCTS</h3>
-      <div className="item1">
+      <div
+        onClick={() => {
+          setSts(true);
+        }}
+        className="item1"
+      >
         <img src="a" alt="Product"></img>
         <p className="it">Name</p>
         <p>Price</p>
@@ -16,48 +132,17 @@ function Myproducts() {
           width={15}
         />
       </div>
-      <div className="addproduct">
-        <h5>ADD PRODUCT</h5>
-        <div className="prodinfo">
-          <div className="prod1">
-            <div className="prodfield">
-              <p>Product Name</p>
-              <input type="text" placeholder="Product Name"></input>
-            </div>
-            <div className="prodfield">
-              <p>Description</p>
-              <textarea placeholder="Description"></textarea>
-            </div>
-          </div>
-          <div className="prod2">
-            <div className="prodfield">
-              <p>Price</p>
-              <input type="text" placeholder="Price Name"></input>
-            </div>
-
-            <div className="prodfield">
-              <p>Image Url</p>
-              <input type="text" placeholder="URL"></input>
-            </div>
-            <div className="dropdown">
-              <div className="prodfield">
-                <p>Status</p>
-                <select>
-                  <option value="Available">Available</option>
-                  <option value="Not In Stock">Not In Stock</option>
-                </select>
-              </div>
-              <div className="prodfield">
-                <p>Category</p>
-                <select>
-                  <option value="Smartphone">Smartphone</option>
-                </select>
-              </div>
-            </div>
-          </div>
-        </div>
-        <button>ADD PRODUCT</button>
-      </div>
+      {sts ? (
+        <Productform
+          purpose={"UPDATE"}
+          details={["name", "price", "desc", "image", "status", "category"]}
+          status={setSts}
+        ></Productform>
+      ) : (
+        ""
+      )}
+      <div className="updateproduct"></div>
+      <Productform purpose={"ADD"} details={""}></Productform>
     </div>
   );
 }
