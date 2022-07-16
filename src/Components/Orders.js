6881d4b8d7a76fa285b52e9069c54e8f947b4a0d
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-16 21:10:52 +0530

    orders

--- src/Components/Orders.js
+++ src/Components/Orders.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Orders.css";
+import { useLocation } from "react-router-dom";
+import { LoginContext } from "../context";
 
 function Orders() {
+  const { token } = useContext(LoginContext);
+  const { state } = useLocation();
+  const { orders } = state;
+  console.log(orders);
   return (
     <div>
       <h3 className="infohead">ORDERS</h3>
-      <div className="item2">
-        <img src="a" alt="Product"></img>
-        <p className="it">Name</p>
-        <p>Price</p>
-        <p>Quantity</p>
-        <p>Date</p>
-        <p>Delivered</p>
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/1215/1215092.png"
-          alt="Delete"
-          width={15}
-        />
-      </div>
+      {orders
+        ? orders.map((obj) => (
+            <div className="item2">
+              <img src={obj.products.image} alt="Product" width={80}></img>
+              <p
+                className="it"
+                style={{ fontSize: "20px", marginRight: "0px" }}
+              >
+                {obj.products.name}
+              </p>
+              <p>{obj.products.price * obj.quantity}</p>
+              <p>{obj.quantity}</p>
+              <p>{obj.delivered ? " Delivered" : "Not Delivered"}</p>
+              <img
+                style={{ marginRight: "15px" }}
+                src="https://cdn-icons-png.flaticon.com/512/1215/1215092.png"
+                alt="Delete"
+                width={15}
+              />
+            </div>
+          ))
+        : ""}
     </div>
   );
 }
