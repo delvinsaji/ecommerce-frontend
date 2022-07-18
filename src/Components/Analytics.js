1cd8b429da8454536120f7700e9a2fde15687bbf
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-18 18:49:46 +0530

    analytics

--- src/Components/Analytics.js
+++ src/Components/Analytics.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./Analytics.css";
+import { LineChart } from "recharts";
+import axios from "axios";
+import { LoginContext } from "../context";
 
 function Analytics() {
+  const [data, setData] = useState();
+  const [total, setTotal] = useState();
+  const [line, setLine] = useState([]);
+  const { token } = useContext(LoginContext);
+  useEffect(() => {
+    axios
+      .get(
+        `https://e1commerce.herokuapp.com/api/visitedanalytics/${token[0]}/`,
+        {
+          headers: {
+            Authorization: `Bearer ${token[1]}`,
+          },
+        }
+      )
+      .then((Response) => {
+        setData(Response.data);
+        setTotal(data[Response.data.length - 1]);
+      })
+      .catch((error) => {
+        alert(error.data);
+      });
+  }, []);
+
   return <div></div>;
 }
 
