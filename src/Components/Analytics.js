fa21ef2e72d5bf03427f89013b456a9e6bb71b9e
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-21 15:52:47 +0530

    final

--- src/Components/Analytics.js
+++ src/Components/Analytics.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState, useContext } from "react";
 import "./Analytics.css";
-import { LineChart } from "recharts";
 import axios from "axios";
 import { LoginContext } from "../context";
 
 function Analytics() {
-  const [data, setData] = useState();
-  const [total, setTotal] = useState();
-  const [line, setLine] = useState([]);
+  const [vdata, setVdata] = useState();
+  const [odata, setOdata] = useState();
+  const [vtotal, setVtotal] = useState();
+  const [ototal, setOtotal] = useState();
   const { token } = useContext(LoginContext);
+  console.log(token);
   useEffect(() => {
     axios
       .get(
@@ -20,15 +21,72 @@ function Analytics() {
         }
       )
       .then((Response) => {
-        setData(Response.data);
-        setTotal(data[Response.data.length - 1]);
+        setVdata(Response.data);
+        setVtotal(vdata[Response.data.length - 1]);
+      })
+      .catch((error) => {
+        alert(error.data);
+      });
+    axios
+      .get(
+        `https://e1commerce.herokuapp.com/api/ordersanalytics/${token[0]}/`,
+        {
+          headers: {
+            Authorization: `Bearer ${token[1]}`,
+          },
+        }
+      )
+      .then((Response) => {
+        setOdata(Response.data);
+        setOtotal(odata[Response.data.length - 1]);
       })
       .catch((error) => {
         alert(error.data);
       });
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      <h3 style={{ fontWeight: "normal" }}>
+        Numbers of users visited my products
+      </h3>
+      <table style={{ border: "1px solid white" }}>
+        <tr>
+          <th>Date</th>
+          <th>User count</th>
+        </tr>
+        {vtotal
+          ? Object.keys(vtotal).map((obj, index) => (
+              <tr>
+                <td style={{ border: "1px solid white" }}>{obj}</td>
+                <td style={{ textAlign: "center", border: "1px solid white" }}>
+                  {vtotal[obj]}
+                </td>
+              </tr>
+            ))
+          : ""}
+      </table>
+      <h3 style={{ fontWeight: "normal" }}>
+        Numbers of users ordered my products
+      </h3>
+      <table style={{ border: "1px solid white" }}>
+        <tr>
+          <th>Date</th>
+          <th>User count</th>
+        </tr>
+        {ototal
+          ? Object.keys(ototal).map((obj, index) => (
+              <tr>
+                <td style={{ border: "1px solid white" }}>{obj}</td>
+                <td style={{ textAlign: "center", border: "1px solid white" }}>
+                  {ototal[obj]}
+                </td>
+              </tr>
+            ))
+          : ""}
+      </table>
+    </div>
+  );
 }
 
 export default Analytics;
